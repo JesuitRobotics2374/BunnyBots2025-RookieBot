920bbcc8da994cd371c1242b60079d3e5aaf3786
NW <jhiramat28@jesuitmail.org>
2025-10-14 17:49:38 -0700

    defining functions
    we made function shells for the future, will be changed with designs input.
    
    -Divit and Joey

---
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -0,0 +1,40 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class ShooterSubsystem extends SubsystemBase {
+  private final targetRPM = 12; //change later when more info is available 
+
+  public ShooterSubsystem() {
+
+    public boolean isCarrotLoaded() {
+
+    }
+
+    public boolean checkRpmShooter() {
+
+    }
+
+    public void setRpmShooter(int rpmValue) {
+      
+    }
+
+    public boolean isShooterReady(boolean isCarrotLoaded()) {
+
+    }
+
+    public void shootCarrot(int rpmValue) {
+
+    } //unfinished untill thursday
+
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
