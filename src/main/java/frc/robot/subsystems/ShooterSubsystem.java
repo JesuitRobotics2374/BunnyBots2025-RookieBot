0ad5d986e7ca30eb857c3404e260a4f47e96b8c2
mxmotter <mmotter28@jesuitmail.org>
2025-10-16 16:36:14 -0700

    progress check

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -5,31 +5,56 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.*;
 
 public class ShooterSubsystem extends SubsystemBase {
   private final targetRPM = 12; //change later when more info is available 
+  // motor type for flywheels is kraken 
+  private TalonFX control;
 
   public ShooterSubsystem() {
 
-    public boolean isCarrotLoaded() {
+    this.control = new TalonFX(37, "FastFD"); // change id when necessary
 
-    }
 
     public boolean checkRpmShooter() {
+      int motorRPS = control.getRotorVelocity().getValue();
+      return motorRPS * 60 == targetRPM;
+      // use this for shuffleboard 
 
     }
 
     public void setRpmShooter(int rpmValue) {
+      control.set(rpmValue);
+    }
+
+    public boolean isShooterReady(boolean isCarrotLoaded) {
+
+      // indexer and shooter need to communicate about if 5 carrots are ready to be shot
+
+      if (checkRpmShooter()  /* && insert communication here*/ ) {
+        return true;
+      }
+      else {
+        return false;
+      }
+
+
+    }
+
+    public void shootCarrots(int rpmValue) {
       
-    }
+      setRpmShooter(targetRPM);
+      // wait however long it takes for flywheel to reach targetRPM
+      
 
-    public boolean isShooterReady(boolean isCarrotLoaded()) {
+    
 
-    }
 
-    public void shootCarrot(int rpmValue) {
+    // fix later
 
-    } //unfinished untill thursday
+    } 
 
   }
 
